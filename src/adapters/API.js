6d767e451dc43957e2e67bccb97cca65b5fc05a4
Avelyne0 <sam.dbarrie@gmail.com>
2019-08-21 10:24:31 +0100

    added url switcher

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,5 +1,10 @@
-// const endpoint = 'http://localhost:3000/api/v1'
-const endpoint = 'https://bardic-inspiration-backend.herokuapp.com/api/v1'
+let endpoint = null
+if (window.location.href.includes('heroku')) {
+    endpoint = 'https://bardic-inspiration-backend.herokuapp.com/api/v1';
+  } else {
+    endpoint = 'http://localhost:3000/api/v1'
+  }
+  
 const signupUrl = `${endpoint}/users`
 const loginUrl = `${endpoint}/login`
 const charactersUrl = `${endpoint}/characters`
